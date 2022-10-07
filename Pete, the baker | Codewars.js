90672ec16a54c920916356bf9a50db131d9fdde0
Karem Mohamed <karemkero21@gmail.com>
2022-10-07 12:00:30 +0200

    Create Pete, the baker | Codewars.js

---
+++ Pete, the baker | Codewars.js
@@ -0,0 +1,17 @@
+// Problem Solution
+// Define variable minimal count
+// loop throw the keys of recipes object
+// Check if the ingredient available, if it isn't return 0
+// compare the amount made of the current ingredient to the minial count made
+// if there isn't minimal count or amountMade < minimalCount, then minimalCount = amountMade
+
+function cakes(recipe, available) {
+  let minimalCount; 
+  for (let key in recipe) {
+    if (!available[key]) return 0;
+    let amountMade = Math.floor(available[key] / recipe[key]);  
+    if (!minimalCount || amountMade < minimalCount) minimalCount = amountMade;
+  }
+  
+  return minimalCount;
+}
