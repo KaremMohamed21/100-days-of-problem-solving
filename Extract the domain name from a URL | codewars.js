74138eef451123e563ce32a72a8dbc1cf6dd5a90
Karem Mohamed <karemkero21@gmail.com>
2022-10-14 13:42:54 +0200

    Create Extract the domain name from a URL | codewars.js

---
+++ Extract the domain name from a URL | codewars.js
@@ -0,0 +1,7 @@
+// Problem Solution
+function domainName(url){
+  url = url.replace("https://", '');
+  url = url.replace("http://", '');
+  url = url.replace("www.", '');
+  return url.split('.')[0];
+};
